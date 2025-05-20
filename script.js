a51a68d55095851b8a95f6df52cf3ccaa427e8fe
sharmababita <sharma.089babita@gmail.com>
2025-05-20 13:14:47 +0200

    DOM

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+console.log("Hello World!");
+
+// Datatyper & Variabler
+const userName = "Julie"; // String
+const userAge = 28; // Number
+let userPresent = true; // Boolean
+console.log(
+  "Mitt navn er " +
+    userName +
+    ". Jeg er " +
+    userAge +
+    " år gammel. Og det er " +
+    userPresent +
+    " at jeg er tilstede på Fontenehuset i dag!"
+);
+
+// Regular function
+function helloFunction() {
+  console.log("Hello from inside function!");
+}
+helloFunction();
